d4df27bab64d66e1fe22466dafaf248525e870b2
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-15 18:21:30 -0600

    add: dart config as well as some personal plugins

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 --[[
+{ import }
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -41,8 +42,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("custom.remap")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
@@ -75,6 +75,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  "Pocco81/auto-save.nvim",
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -85,16 +86,14 @@ require('lazy').setup({
       -- Automatically install LSPs to stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
-
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
   },
-
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -124,7 +123,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -199,7 +198,11 @@ require('lazy').setup({
       end,
     },
   },
-
+  {
+    "thePrimeagen/harpoon",
+    branch = 'harpoon2',
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
@@ -273,8 +276,8 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -282,8 +285,9 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
+
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -507,6 +511,13 @@ vim.defer_fn(function()
   }
 end, 0)
 
+require 'lspconfig'.dartls.setup {
+  cmd = { "dart", 'language-server', '--protocol=lsp' },
+}
+
+
+
+
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
@@ -627,6 +638,12 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+
+--[[
+--continue after dinner add the primeagens plugins as well as the fugitive config & we should
+-- have a good starting point, also do configs & we'll be pretty close
+--
+--]]
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
