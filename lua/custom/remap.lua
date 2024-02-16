fb342b6b28e2510eac1326966294569cb4212ee9
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-15 18:26:06 -0600

    move: remap to fugitive config (cleanliness)

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -26,7 +26,6 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 
 vim.keymap.set("n", "<leader><leader>", function()
