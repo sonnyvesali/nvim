b0dd298ae397298936f73a38e69a3fa7cd269ac8
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-17 12:41:51 -0600

    lsp org

---
+++ lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,10 @@
+return {
+	'neovim/nvim-lspconfig',
+
+	dependencies = {
+		{ 'williamboman/mason.nvim', config = true },
+		'williamboman/mason-lspconfig.nvim',
+		{ 'j-hui/fidget.nvim',       opts = {} },
+		'folke/neodev.nvim',
+	},
+}
