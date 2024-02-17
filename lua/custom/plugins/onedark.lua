a5cdd1d745598562d21e4d8a5098057237efdbb4
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-17 12:41:20 -0600

    onedark plugin config

---
+++ lua/custom/plugins/onedark.lua
@@ -0,0 +1,12 @@
+return {
+	'navarasu/onedark.nvim',
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require('onedark').setup {
+			style = 'darker',
+			transparent = true,
+		}
+		require('onedark').load()
+	end,
+}
