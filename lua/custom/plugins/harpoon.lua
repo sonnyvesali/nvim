35edc737fe92b10ba2bd7f6384e1ebfc5c3462b3
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-17 12:37:40 -0600

    harpoon organization

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,13 +1,5 @@
-local harpoon = require("harpoon")
-
-
-harpoon:setup()
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
-return {}
+return {
+	"thePrimeagen/harpoon",
+	branch = 'harpoon2',
+	dependencies = { "nvim-lua/plenary.nvim" }
+}
