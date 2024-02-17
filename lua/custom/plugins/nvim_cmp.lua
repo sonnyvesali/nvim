719de3498073cc8448f169000dce645e49bdefcb
Sonny Vesali <sonny.vesali@protonmail.com>
2024-02-17 12:41:33 -0600

    nvim_cmp plugin config org

---
+++ lua/custom/plugins/nvim_cmp.lua
@@ -0,0 +1,19 @@
+return {
+	'hrsh7th/nvim-cmp',
+	dependencies = {
+		{
+			'L3MON4D3/LuaSnip',
+			build = (function()
+				if vim.fn.has 'win32' == 1 then
+					return
+				end
+				return 'make install_jsregexp'
+			end)(),
+		},
+		'saadparwaiz1/cmp_luasnip',
+		"Exafunction/codeium.nvim",
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-path',
+		'rafamadriz/friendly-snippets',
+	},
+}
